114a1f02e60951ec99946508deb50bb9a2caf4a1
Insaram Alam <alaminsaram92@gmail.com>
2022-02-11 01:09:28 +0500

    Qr Code added

--- src/Screen/QrCode.js
+++ src/Screen/QrCode.js
@@ -1,14 +1,55 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import { StyleSheet, Text, View, Button, SafeAreaView } from "react-native";
+import React, { useEffect, useState } from "react";
+import { BarCodeScanner } from "expo-barcode-scanner";
+import { StatusBar } from 'expo-status-bar';
+
 
 const QrCode = () => {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    console.log(data);
+    // alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
-    <View>
-      <Text>QrCode</Text>
+    <View style={{ flex: 1 }}>
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={StyleSheet.absoluteFillObject}
+      />
+      {scanned && (
+        <View style={styles.button}>
+          <Button
+            title={"Tap to Scan Again"}
+            onPress={() => setScanned(false)}
+          />
+        </View>
+      )}
+      <StatusBar style="dark" />
+      
     </View>
   );
 };
 
 export default QrCode;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  button: { position: "absolute", bottom: 20, alignSelf: "center" },
+});
